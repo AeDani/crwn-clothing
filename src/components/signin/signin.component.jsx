ef4fc1a2afc69a0b7fe9747eb9f2b8ed15282cff
AeDani <31882578+AeDani@users.noreply.github.com>
2020-03-28 20:45:00 +0100

    finished sign in with email and password

--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
@@ -16,9 +16,16 @@ class SignIn extends React.Component {
 		};
 	}
 
-	handleSubmit = e => {
+	handleSubmit = async e => {
 		e.preventDefault();
-		this.setState({ email: '', password: '' });
+		const { email, password } = this.state;
+
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({ email: '', password: '' });
+		} catch (error) {
+			console.error(error);
+		}
 	};
 
 	handleChange = e => {
