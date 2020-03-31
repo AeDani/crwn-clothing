99adc09cd4939e4e0f4374f7be9f492ff3a5ba17
AeDani <31882578+AeDani@users.noreply.github.com>
2020-03-31 21:01:32 +0200

    implemented redirect

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,10 @@
+import { UserActionTypes } from './user.types';
+
+console.log(UserActionTypes.SET_CURRENT_STATE);
+
 export const setCurrentUser = user => {
 	return {
-		type: 'SET_CURRENT_USER',
+		type: UserActionTypes.SET_CURRENT_STATE,
 		payload: user
 	};
 };
